a88145a0e3b39ea568f71166d05592ab5c5d8ae9
jensaugustmb@hotmail.com <jensaugustmb@hotmail.com>
2020-11-27 13:32:27 +0100

    Add project files.

---
+++ readcsv/Program.cs
@@ -0,0 +1,83 @@
+﻿using readcsv;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] read;
+            char[] seperators = { ',' };
+            List<politiker> folketing = new List<politiker>();
+            StreamReader sr = new StreamReader($"C:/Users/jensa/OneDrive/Dokumenter/folketing/Folketing.csv");
+            {
+                string navn1 = "";
+                string parti1 = "";
+                string email1 = "";
+                string mobil1 = "";
+
+                string data = sr.ReadLine();
+
+                while ((data = sr.ReadLine()) != null)
+                {
+                    // read = data.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+                    read = data.Split(seperators);
+
+                    if (read.Count() > 3)
+                    {
+                        navn1 = read[0];
+                        parti1 = read[1];
+                        email1 = read[2];
+                        mobil1 = read[3];
+                    }
+                    else
+                    {
+                        navn1 = read[0];
+                        parti1 = read[1];
+                        email1 = read[2];
+                        mobil1 = "";
+                    }
+                    
+                    // getantalinstancer er en static metode og derfor 
+                    folketing.Add(new politiker() { navn = navn1, parti = parti1, email = email1, mobil = mobil1 });
+                    Console.WriteLine("Antal politikere: " + politiker.getinstancer());
+
+                    Console.WriteLine("Data fra CSV {0} {1} {2} {3}", navn1, parti1, email1, mobil1);
+                }
+            }
+            int stemnej = 0;
+            int stemja =0;
+            foreach (politiker item in folketing)
+            {
+                if (politiker.Afstemning() == false)
+                {
+                    stemnej++;
+                }
+                else
+                {
+                    stemja++;
+                }
+                //Console.WriteLine("Afstemning: " + politiker.Afstemning());
+            }
+            if (stemja > stemnej)
+            {
+                Console.WriteLine("\nLovforslaget er hermed blevet vedtaget!");
+            }
+            if (stemnej > stemja)
+            {
+                Console.WriteLine("\nLovforslaget blev desværre forkastet");
+            }
+            if (stemnej==stemja)
+            {
+                Console.WriteLine("\nDet står lige! Der bliver nød til at ske en ny afstemning");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readcsv/politiker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readcsv
+{
+    class politiker
+    {
+        public string navn;
+        public string parti { get; set; }
+        public string email;
+        public string mobil;
+
+        static int _antalinstancer = 0;
+
+     
+        public override string ToString() // skal overrides fra den arvede klasse - object.tostring()
+        {
+            return "lokal tostring () - hej fra instansen";
+        }
+        public politiker()
+        {
+            _antalinstancer++;
+        }
+        static public int getinstancer()
+        {
+            return _antalinstancer;
+        }
+        public static bool Afstemning()
+        {
+            Random random = new Random();
+            int Random = random.Next(10);
+            if (Random >= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
